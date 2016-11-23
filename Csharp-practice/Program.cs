053b8b1c28fa0eb2b2878a345e1504ed2e960cd0
Sterling <elmoresterling@yahoo.com>
2016-11-22 21:52:51 -0600

    Created script that reads two integers from user input and print the integers before the swap and after the swap

--- Csharp-practice/Program.cs
+++ Csharp-practice/Program.cs
@@ -10,11 +10,21 @@ namespace Csharp_practice
     {
         public static void number()
         {
-            int n1;
-            Console.Write("Enter number: ");
+            int n1, n2, temp;
+            Console.Write("Enter first number: ");
             n1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("The number you entered is: {0}", n1);
+            Console.Write("Enter second number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            //Before swap
+            Console.WriteLine("{0} , {1}", n1, n2);
+            //swap the two integers
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+            //after swap
+            Console.WriteLine("{0} , {1}", n1, n2);
         }
         static void Main(string[] args)
         {
